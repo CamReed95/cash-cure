b60dc16413868f00f656bb5be3c5bf3dcd453e19
Cameron Reed <cameronreedadjusting@gmail.com>
2020-05-26 16:28:55 -0700

    Adjusted file names, content - in process of writing tests for code - recommend completing components before proceeding

---
+++ src/__tests__/functions.test.js
@@ -0,0 +1,10 @@
+import {add} from '../utilities/functions';
+
+
+// describe('tests for add function, () => {
+
+// test('add 1 + 2 and expect it to be 3', () => {
+//     expect (add(1,2)).toBe(3);
+// })
+
+// })
--- src/__tests__/income.test.js
+++ src/__tests__/income.test.js
@@ -2,3 +2,10 @@ import React, {Component} from 'react';
 import {render, fireEvent} from '@testing-library/react';
 import Counter from '../Components/Income';
 
+describe('testing for the Income component', () => {
+
+    it('Renders out starting text', () => {
+        const {}
+    })
+
+})
