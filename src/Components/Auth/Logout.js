378aac46ab62770ed4d93754b948823437010d54
Cameron Reed <cameronreedadjusting@gmail.com>
2020-05-27 10:57:07 -0700

    wrote logout and register auth files

---
+++ src/Components/Auth/Logout.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {logout} from '../../redux/User'  
+
+class Logout extends React.Component {
+    constructor(props) {
+        super(props)
+        this.logoutHandler = this.logoutHandler.bind(this)
+    }
+    logoutHandler(){
+        this.props.logout().then(() => {
+
+        }).catch(err => console.log('Error Logging out', err))
+    }
+    render() {
+        return(
+            <div>
+                <button className='logout-button-2' onClick={this.logoutHandler}
+                >Signout</button>
+                <div className='Welcome-username'>Welcome, {this.props.user.data.email}
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => state
+
+const mapDispatchToProps = {logout}
+
+export default connect (mapStateToProps, mapDispatchToProps)(Logout);
