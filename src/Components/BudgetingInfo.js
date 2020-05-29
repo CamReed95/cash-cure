e0ac68f90b015c14ed166b58e90029f7ea772dd2
Cameron Reed <cameronreedadjusting@gmail.com>
2020-05-29 07:40:06 -0700

    Finished budgeting info comp

--- src/Components/BudgetingInfo.js
+++ src/Components/BudgetingInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './BudgetingInfo.css';
+import './BudgetingInfo.css';
 import Header from './Header';
 
 const Info = () => {
@@ -7,8 +7,8 @@ const Info = () => {
         <div className="bf-div">
         <Header/>
             <div className="BudgetingInfo">
-                    <h1>The Benefits of Budgeting</h1>
-                <p>Text goes here</p>
+                    <h1 className="benefits">Budgeting Benefits: </h1>
+                    <p className="info-text">What is a budget? <br/>According to Investopedia, a budget is an estimation of revenue and expenses over a specified future period of time and is usually compiled and re-evaluated on a periodic basis. <br/>A little complicated? Don't worry, we'll break down the pros of what all of this means. The following are only SOME of the benefits of budgeting: <br/>- Control your money, not the other way around <br/>- Ends needless spending <br/>- Creates a financial plan to help accomplish your goals <br/>- Allows preparation for emergencies or worthwhile experiences <br/>- The list goes on!</p>
             </div>
         </div>
     )
